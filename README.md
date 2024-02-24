0387a4507289bd38ab725cc468f31ebb5749bc14
Nathaniel Watts <reg@nathanielwatts.com>
2024-02-23 22:42:50 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Treesitter Bug?
+
+It looks like, if `highlight = { enable = true }` is set, that `gf` breaks.
+
+### To reproduce:
+
+1. Startup NVIM, using the provided bare-bones `init.lua`.
+
+( Note: It *only* has lazy-vim, vim-rails, and nvim-treesitter )
+
+2. cd into the included app directory, and open `app/views/posts/_post.html.erb`
+3. Move your cursor to be over the word "another_partial"
+4. In normal mode, hit `gf`
+
+#### Expected:
+
+The file, `app/views/posts/_another_partial.html.erb` will be opened.
+
+#### Actual:
+
+You get the error: `Cannot find "another_parial" in path`
+
+---
+
+If you change the config in `init.lua` to set `highlight` to `enable = false` - it works as expected.
---
+++ init.lua
@@ -0,0 +1,30 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	"tpope/vim-rails",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				sync_install = false,
+				highlight = { enable = true },
+				-- highlight = { enable = false }, # use me instead, things work
+				indent = { enable = true },
+			})
+		end,
+	},
+})
